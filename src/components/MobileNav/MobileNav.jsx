efde017c8357ba9657170a4942679da871f66d23
4002-Nonye <royaltysungbin@gmail.com>
2023-09-15 19:23:54 -0700

    final commit

--- src/components/MobileNav/MobileNav.jsx
+++ src/components/MobileNav/MobileNav.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import SideBar from '../SideBar/SideBar'
 import ham from "../../assets/Menu.svg";
 import logo from "../../assets/logodark.svg";
 import styles from "./MobileNav.module.css";
@@ -8,6 +9,9 @@ function MobileNav() {
     <nav className={styles.nav}>
       <img src={logo} alt="logo" />
       <img src={ham} alt="ham" />
+
+
+      <SideBar/>
     </nav>
   );
 }
